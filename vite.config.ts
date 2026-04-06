432f0b8fb79a41de1af18d67e136fae22742341c
cbctopgt6-cpu <cbctopgt6@gmail.com>
2026-04-06 18:37:45 +0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/dental/',   // 👈 এইটা add করুন
-
       server: {
         port: 3000,
         host: '0.0.0.0',
